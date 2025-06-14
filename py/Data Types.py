659975476f9a8e8efc1ca8eed90922d82daa3384
Ahad <ahadbutt22001@gmail.com>
2025-06-14 16:23:00 +0500

    2nd

---
+++ py/Data Types.py
@@ -0,0 +1,9 @@
+a=1         #Integer
+b="Ahad"    #String
+c=True      #boolen
+d=None      #None typw
+
+print("Type of a is ",type(a))
+print("Type of b is ",type(b))
+print("Type of c is ",type(c))
+print("Type of d is ",type(d))
