8d8232f7df60ab93f4a93d7330d269eb2058d161
Ahad <ahadbutt22001@gmail.com>
2025-06-14 15:01:29 +0500

    Add files via upload

---
+++ py/1st program.py
@@ -0,0 +1,4 @@
+print("Hello World")
+print(532)
+print("Ke hall aa Theek oo na")
+print(17*13)
---
+++ py/pips and modules.py
@@ -0,0 +1,2 @@
+print("Modules are like librays in Cpp provide built in functions and pips are it managers and are used to download modules");
+# pip install pandas
