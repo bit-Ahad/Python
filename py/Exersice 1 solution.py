a418701e9e3669c52ea0d26a62fc8b3bc702addf
Ahad <ahadbutt22001@gmail.com>
2025-06-15 12:02:02 +0500

    Add files via upload
    3rd

---
+++ py/Exersice 1 solution.py
@@ -0,0 +1,8 @@
+# Calculator
+print("Following program perform basic arithmetic operations")
+a=29764
+b=2134
+print("Addition of 2 number is",a+b)
+print("Subtraction of 2 number is",a-b)
+print("Multiplication of 2 number is",a*b)
+print("Division of 2 number is",a/b)
---
+++ py/Operator and Exersice 1.py
@@ -0,0 +1,5 @@
+# // is floor divison operator it remove the value after point
+print(3459//93)
+
+# exponential operator is ** that represent power (5**3 == 5 power 3)
+print(5**3)
