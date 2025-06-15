2c2932dc030563e7bc1803c6ab66a6ccf86249e4
Ahad <ahadbutt22001@gmail.com>
2025-06-15 12:22:21 +0500

    Add files via upload

---
+++ py/Typecasting.py
@@ -0,0 +1,11 @@
+# Explict type casting is the one that is done by user
+a = "1"
+b = "2"
+
+print('Sum of both is', int(a)+int(b))
+
+# Implict type casting is done by python itself
+c=9.43
+d=3
+
+print("Automatically converted to float by python",c+d)
