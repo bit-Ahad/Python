ca5571675d54a677ba33003707f94d170f7eeeda
Ahad <ahadbutt22001@gmail.com>
2025-06-15 15:20:08 +0500

    Add files via upload

---
+++ py/input.py
@@ -0,0 +1,12 @@
+# input() is used to take input from user
+
+a=input('Enter any datatype value')
+print('a has stored',a)
+# Input take input as a string
+print('Datatype of a is',type(a))
+
+x=int(input("Enter 1st number"))
+y=int(input("Enter 2nd number"))
+print('Sum of both numbers is',x+y)
+print('Data type of x is',type(x))
+print('Data type of y is',type(y))
---
+++ py/string.py
@@ -0,0 +1,17 @@
+# Multi line string
+a = "AMAZING"
+b = """GOOING
+TO
+NEW
+LINE
+okay!!!!! """
+
+# Using Indexs in string
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+
+# Using for character loop
+for character in b:
+    print(character)
