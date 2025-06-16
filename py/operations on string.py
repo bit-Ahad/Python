5cfe14843eac305c8c82d6502e0626ad3b6c1916
Ahad <ahadbutt22001@gmail.com>
2025-06-16 16:39:13 +0500

    Add files via upload

---
+++ py/operations on string.py
@@ -0,0 +1,15 @@
+# Indexs with string
+name = "Ahad,AHad,Ahad"
+print(name[0:5]) #It include 0 but not 5 indexs
+
+# Find lenght of string
+print(len(name))
+
+# String slicing
+fruit = "Apple"
+print(fruit[0:4])   #Give according to indexes
+print(fruit[:3])    #Give from 0 to 3
+print(fruit[:])     #Give all charcters
+
+print(fruit[0:-3])  #it minus 3 from from total lenght...That make index from 0 to 2 like [0:2]
+print(fruit[-3:-1])  #it minus both from from total lenght...That make index from 2 to 4 like [2:4]
