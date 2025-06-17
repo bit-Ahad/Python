be8b2c2ed3141dd91c6bfff168cf5e3e00a19d34
Ahad <ahadbutt22001@gmail.com>
2025-06-17 17:01:26 +0500

    Add files via upload

---
+++ py/Forloop.py
@@ -0,0 +1,11 @@
+#  In python for loop is also known as for each loop
+st = "Hello Brother"
+# Simple For Loop
+for characters in st:
+    print(characters)
+# Using range(stop)
+for j in range(5):
+    print(j)
+# Using Range with start stop and steps
+for l in range(2,23,2):
+    print(l)
---
+++ py/while-Loop.py
@@ -0,0 +1,12 @@
+# While loop
+i = int(input("ENter the number"))
+while(i<10):
+    print(i," Fine!!!!")
+    i=i+1;
+
+k = 10;
+while(k>=3):
+    print(k," Here")
+    k=k-1
+else:
+    print("Else will always run")
