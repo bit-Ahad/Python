0aba8b8691b0d0d6b4da9b0d8e9aa7e539ed3db6
Ahad <ahadbutt22001@gmail.com>
2025-06-17 12:38:35 +0500

    Add files via upload

---
+++ py/if--else.py
@@ -0,0 +1,19 @@
+a = int(input("Enter your age : "))
+# IF ELSE 
+print("Single IF Else")
+if(a>=18):
+    print("You can drive..It's fine")
+    print("Still in the IF condition")
+else:
+    print("You CANNOT drive..It's NOT fine")
+# MULTIPLE IF ELSE
+print("MUltiple if else")
+if(a>=18):
+    print("Okay !!!!")
+elif(a<10):
+    print("Bero You are not even 10")
+elif(a<=0):
+    print("First you need to be born")
+else:
+    print("Chutti Kr")
+# Nested IF Else is IF ELSE in another IF ELSE just
