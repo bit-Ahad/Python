b448b7f6410e20941aae1e347840cca1985f2804
Ahad <ahadbutt22001@gmail.com>
2025-06-20 18:05:02 +0500

    Add files via upload

---
+++ py/forloop with else.py
@@ -0,0 +1,11 @@
+# Else will always run if any loop is finished without break 
+for i in range(5):
+    print(i)
+else:
+    print("Maximum has reached")
+
+li = []
+for i in li:
+    print(i)
+else:
+    print("Sorry list is empty")
