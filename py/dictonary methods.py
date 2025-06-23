a679a8b8a66d73db3e84d252578de7c3e4508fb7
Ahad <ahadbutt22001@gmail.com>
2025-06-23 15:43:37 +0500

    Add files via upload

---
+++ py/dictonary methods.py
@@ -0,0 +1,14 @@
+di1 = {1:23,2:64,3:56,4:84,5:44}
+di2 = {5:88,6:13,7:34,8:96}
+
+# Just like unioun but it replace the duplicate one
+di1.update(di2)
+print(di1)
+
+# di2.clear()
+# print("Di2 is cleared : ",di2)
+
+di2.pop(8)
+print(di2)
+
+# del di2 
