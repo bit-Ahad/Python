3c0647c7c1261613ba52f107e1fc46a928c579a8
Ahad <ahadbutt22001@gmail.com>
2025-06-23 15:17:23 +0500

    Add files via upload

---
+++ py/dictonary.py
@@ -0,0 +1,13 @@
+di = {"name":"AHad","age":23,"class":43}
+
+print(di)
+print(di["name"])
+
+print(di.keys())
+
+print(di.values())
+
+print(di.items())
+
+for key in (di):
+    print(f"The value for {key} is {di[key]}")
