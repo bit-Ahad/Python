e6db64e7e6f1f1ee05897162aab79c69ab227b62
Ahad <ahadbutt22001@gmail.com>
2025-06-27 15:30:37 +0500

    Add files via upload

---
+++ py/makedirectory.py
@@ -0,0 +1,15 @@
+import os
+
+if (not os.path.exists("Ahad")):
+    os.mkdir("Ahad")
+
+if (not os.path.exists("os module/Testing")):
+    for i in range(1,6):
+      os.mkdir(f"os module/Testing/File{i}")
+
+print(os.listdir("os module\Testing"))
+
+folders = os.listdir("os module/Testing")
+for folder in folders:
+   print(folder)
+   print(os.listdir(f"os module/Testing/{folder}"))
