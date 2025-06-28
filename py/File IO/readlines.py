84eb6c5fd2a1567f7609bb2c3cb192c92737c541
Ahad <ahadbutt22001@gmail.com>
2025-06-28 20:28:45 +0500

    Add files via upload

---
+++ py/File IO/readlines.py
@@ -0,0 +1,15 @@
+# read() is used to read all,readline() read a single line everytime,readlines() give a list of string of all lines 
+# write() used to write all, writelines() is used to write list of strings
+
+# f = open("mytext.txt","w")
+
+# list = ["ABC\n","DEF\n","GHI\n"]
+# f.writeline(list)
+
+# f = open("mytext.txt","r")
+
+# while True:
+#     line = f.readline()
+#     if not (line):
+#         break
+#     print(line)
