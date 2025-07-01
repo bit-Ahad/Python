666413c25f94e883bd298830628e0f9dac1bb662
Ahad <ahadbutt22001@gmail.com>
2025-07-01 11:12:05 +0500

    Update basics.py

--- py/basics.py
+++ py/basics.py
@@ -1,8 +1,12 @@
 print("Comments")
+
 # single line comment
 """Multi line comments"""     '''Triple single or double quote'''
+
 print("Going to \nnew line Bye")
+
 print("Hey",2323,39+577)
+
 print(2,5,9,14,sep="<>")
 print(end="3342")
 print("Wow")
